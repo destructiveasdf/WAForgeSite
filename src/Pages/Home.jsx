6e93ffacc4292a47f5c06e5a94400402230aa55c
deactiveasdf <111084408+destructiveasdf@users.noreply.github.com>
2025-02-20 13:56:04 -0800

    changed videos

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import SectionBreak from '../assets/SectionBreak';
 import video1 from "../assets/Videos/1.mp4";
 import video2 from "../assets/Videos/2.mp4";
 import video3 from "../assets/Videos/3.mp4";
+import video4 from "../assets/Videos/4.mp4";
 import ServiceSection from '../assets/Section';
 import HackathonSection from '../assets/HackathonSection';
 import ImageHorizontalCard from '../assets/ImageCard';
@@ -39,7 +40,7 @@ const Home = () => {
                     <Header />
                     <Banner />
                     
-                    <VideoSection shouldChange={true} videos = {[video1, video2, video3]} isHero = {true} element={
+                    <VideoSection shouldChange={true} videos = {[video1, video2, video3, video4]} isHero = {true} element={
                     
                     <button onClick={() => doSomething()} className='relative inline-flex items-center justify-center p-0.5 mt-10 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-[12.5px] group bg-gradient-to-br from-[#00dc82] via-[#36e4da] to-[#16a79e] group-hover:from-green-400 duration-[0.1s] hover:shadow-[0_0px_15px_0px_rgba(0,255,130,1)] dark:text-white '><span className='font-Montserrat font-light relative transition-all ease-in duration-75 bg-white dark:bg-[#18181b] rounded-[9.5px] px-10 py-2'>Get Started</span></button>
                     
